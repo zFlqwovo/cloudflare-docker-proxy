a3a1cad5449fa4c40f0b1f31314b66874c98f5ef
zFlqw <118718052+zFlqwovo@users.noreply.github.com>
2024-06-30 17:10:48 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.mirror.zflqw.xyz": dockerHub,
+  "quay.mirror.zflqw.xyz": "https://quay.io",
+  "gcr.mirror.zflqw.xyz": "https://gcr.io",
+  "k8s-gcr.mirror.zflqw.xyz": "https://k8s.gcr.io",
+  "k8s.mirror.zflqw.xyz": "https://registry.k8s.io",
+  "ghcr.mirror.zflqw.xyz": "https://ghcr.io",
+  "cloudsmith.mirror.zflqw.xyz": "https://docker.cloudsmith.io",
+  "ecr.mirror.zflqw.xyz": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.mirror.zflqw.xyz": dockerHub,
 };
 
 function routeByHosts(host) {
